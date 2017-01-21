eb4cfa77198d5d768ed9c5b5d6a319cb26e715e4
elzup <guild0105@gmail.com>
2017-01-21 21:28:55 +0900

    Response type all update

--- App/Services/Type.js
+++ App/Services/Type.js
@@ -2,22 +2,41 @@
 
 export type Program = {
   id: number,
+  started_at: string,
+  is_rebroadcast: boolean,
+  channel: Channel,
   work: Work,
   episode: Episode
+}
+
+export type Channel = {
+  id: number,
+  name: string
 }
 
 export type Work = {
   id: number,
   title: string,
+  title_kana: string,
+  media: string,
+  media_text: string,
+  season_name: string,
+  season_name_text: string,
+  released_on: string,
+  released_on_about: string,
+  official_site_url: string,
+  wikipedia_url: string,
+  twitter_username: string,
+  twitter_hashtag: string,
   episodes_count: number,
-  watchers_count: number,
-  records_count: number
+  watchers_count: number
 }
 
 export type Episode = {
   id: number,
   number: number,
   number_text: string,
-  records_count: number,
-  title: string
+  sort_number: number,
+  title: string,
+  records_count: number
 }
