f0f4e705e3e4b72d658380630c0753eec0316730
elzup <guild0105@gmail.com>
2017-02-03 22:51:52 +0900

    Update pod

--- android/app/src/main/java/com/annicteye/MainApplication.java
+++ android/app/src/main/java/com/annicteye/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactMaterialKitPackage(),
             new LottiePackage(),
             new VectorIconsPackage(),
             new ReactNativeI18n(),
